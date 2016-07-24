e09e0232a0fb80c08b3df4a7be96bf032dbac965
wuqiangxjtu <wuqiangxjtu@gmail.com>
2016-07-24 20:01:19 +0800

    add tkey

---
+++ letv/tkey.js
@@ -0,0 +1,126 @@
+var C6O = (function() {
+    var U4 = 37.563E2 > 90.189E2 ? (0x3c, 0x55) : (45.3E3, 64E1) <= (0x5, 25.001E0) ? (0x28, 70.47E1) : (24.2E1, 72.4E0) <= 40.8E+1 ? (0x16, 0x1e0426d83f75) : 74.,
+        M4 = (function(V3, k2) {
+            var T0 = "",
+                I8 = (2., 0x4d) < 0x1e ? (84.8E2, 57.4E+3) : 68.8E3 <= 49. ? 85.412E1 : (0x3f, 69.21E+3) <= (55.7E+2, 7.187E3) ? (0x25, 31.) : (0xa, 35.) > (2, 1.) ? (88.755E0, false) : (27, 18.93E2) < (0x4b, 0x3) ? 45. : 20.4E2;
+            if (V3.length > (12.1E1, 0x2f) <= (97.58E2, 15.114E+2) ? (0x27, 0xc) : (6., 11.43E1) >= 84. ? (52.606E0, 98.67E2) : 52.55E1 >= (0x1, 0x5d) ? 0x5e : (26.7E2, 0x28))
+                for (var h8 = 32.32E2 <= 0x25 ? (44.873E2, 0x5e) : 72 < 0x6 ? (0x1e, 0x11) : (5.564E3, 95.02E+1) <= (16., 0x1f) ? 0x16 : 0x1b > 0x3b ? (37, 0x51) : (0x53, 36.05E0) >= (0x30, 0x61) ? (67.1E+1, 0x6) : 0x3b <= (0x3b, 0x3f) ? 0xd : (0xa, 0x63) < (52.9E+3, 80.) ? 24.2E2 : 97.; h8 > 1;) T0 += (I8 = I8 ? (0x26, 74.) >= (85.81E0, 0x55) ? (3., 53.4E1) : (0x48, 4.4E1) > 0x41 ? (0x3d, 82.) : (0x51, 0x1d) <= (12.3E0, 0xd) ? (25, 78) : 38. < (0x8, 0x10) ? (0x2f, 79.) : (0x37, 0x1d) < 0x4 ? (0xa, 43.7E3) : (53.591E3, 80.481E1) <= 0x24 ? (74.4E1, 74.214E1) : 0x23 > (0x23, 0x48) ? 17.52E3 : (87.527E2, false) : (21., 0x35) >= 27.077E2 ? 0x1a : (0x30, 45.69E3) <= (68.387E2, 29) ? (0xa, 0x41) : 0x8 <= (5., 0x43) ? true : 6.82E3 >= (55E1, 9.72E2) ? (0x1d, 0x9) : (0x63, 86.33E1) < 57.3E2 ? (16.15E+0, 0x1e) : (19.84E1, 18.)) ? V3.charAt(h8) : "@%)eitg)(tDwn".charAt(h8--);
+            return k2 === null ? eval(T0) : k2 ^ V3
+        })("_9(mTe.)ea e(", (17, 15.617E2) <= 0x51 ? (0x10, 53.929E3) : (73.9E2, 64.) >= (0x5f, 6.) ? (0x53, null) : (0xd, 95.27E+1) >= (0x11, 70) ? 45.675E1 : (0x56, 95.2E+0) <= (55., 0x3b) ? 0x2e : 98. > (16., 37E2) ? (37.62E2, 44.86E0) : (14., 82.254E+1) < 0x10 ? 28. : (0xc, 18));
+    return {
+        p6: function(H3) {
+            var y1, m1 = 12.5E2 <= (0x55, 98.) ? (81.79E3, 0x4f) : 30 > 0x4f ? (73.38E1, 23.1E1) : (86.861E1, 78.) < 43.093E1 ? (72, 0x0) : (43.182E2, 0x43) < 0xd ? 87. : (39, 92.262E2) >= (0xa, 0x40) ? (38, 47) : (79.5E1, 0x13) > (84.74E+0, 69.641E1) ? 99 : 14.98E+0,
+                Y5 = U4 > M4,
+                h9;
+            for (; m1 < H3.length;) {
+                h9 = (parseInt(H3.charAt(m1), (4.04E3, 83.03E1) <= (67, 37.) ? (1.2E+2, 0x1f) : (71, 0x58) > (1., 0x5c) ? 99E2 : (52.949E1, 27.35E0) >= 0x1e ? (4.159E1, 15.76E1) : 0x63 < (54., 0xe) ? (40.881E2, 28.7E3) : (31.85E+1, 94.9E1) < (0x58, 23.961E3) ? (0x15, 0x10) : 2.3E3 > (59, 47.535E2) ? (25.4E0, 0x1c) : (87.516E3, 0x4e))).toString((0x1f, 36.48E2) <= (67.069E2, 0x45) ? 67.4E+3 : 0x2c < (17.971E+3, 0x10) ? (34, 15.1E+3) : (26.65E2, 0x29) <= (0x4f, 0xd) ? (33.532E+2, 0x4b) : (0x31, 0.716E+2) > (23., 83.61E1) ? (0x44, 0x4c) : (56.52E1, 0x60) > (58.09E2, 30.1E2) ? (0xa, 40.) : (88.508E2, 0x2));
+                y1 = m1++ == (0x12, 0x48) < 20. ? 65.23E1 : (67, 0x4b) < 0x2f ? (95., 97.) : 4.7E2 > 0x4f ? (0x2c, 0x0) : 0x4f <= (0x5b, 0x38) ? 43.793E+3 : 25.12E2 <= (72., 9.) ? (0x1b, 89) : 94 ? h9.charAt(h9.length - (44.8E2, 96.802E1) <= (71., 0x42) ? 0x25 : (82.61E1, 7.6E1) >= 0x22 ? (43.6E2, 0x1) : 0x16 >= (31.198E2, 64.6E+2) ? 90.1E2 : 74.25E+1) : y1 ^ h9.charAt(h9.length - 0x27 >= 0x5c ? 0x2f : (10.6E1, 99.) > 93.07E3 ? 8.5E2 : (23.8E2, 88.7E3) >= 8 ? (0x41, 0x1) : (98.9E2, 0x24))
+            }
+            return y1 ? !Y5 : Y5
+        }
+    }
+})();
+var _1 = "feda8dd6e0127da88f3487a646fe8a6b",
+    _2 = C6O.p6("39") ? "jjuy9567dfj6bkksomnnghwokjlu0o" : "showmethemoney";
+var L41 = {
+    K: function(I, s) {
+        return I < s
+    },
+    s: function(I, s) {
+        return I % s
+    },
+    Y: function(I, s) {
+        return I ^ s
+    },
+    z: function(I, s) {
+        return I & s
+    },
+    R: function(I, s) {
+        return I === s
+    },
+    p: function(I, s) {
+        return I << s
+    },
+    I: function(I, s) {
+        return I - s
+    },
+    J: function(I, s) {
+        return I == s
+    },
+    e: function(I, s) {
+        return I !== s
+    },
+    U: function(I, s) {
+        return I > s
+    },
+    G: function(I, s) {
+        return I | s
+    },
+    w: function(I, s) {
+        return I >> s
+    },
+    F: function(I, s) {
+        return I != s
+    }
+};
+var Key = {
+    liveKey: _1,
+    APIKey: _2,
+    getAPIKey: function(e) {
+        var _1 = C6O.p6("h87") ? "AES" : "feda8dd6e0127da88f3487a646fe8a6b",
+            _2 = C6O.p6("6452") ? "decrypt" : "z",
+            _3 = C6O.p6("64e3") ? "APIKey" : "showmethemoney",
+            _4 = "toString",
+            _5 = "enc",
+            _6 = C6O.p6("30") ? "Utf8" : "Y",
+            _7 = "error";
+        try {
+            return Encode[_1][_2](e, this[_3])[_4](Encode[_5][_6])
+        } catch (t) {
+            return _7
+        }
+    },
+    getCl: function(e) {
+        var _1 = C6O.p6("56f") ? "MD5" : "Utf8",
+            _2 = "showmethemoney",
+            _3 = C6O.p6("3370") ? "error" : "z";
+        try {
+            return Encode[_1](e + _2)
+        } catch (t) {
+            return _3
+        }
+    },
+    getMmsKey: function(e) {
+        var _1 = "s",
+            _2 = "rotateRight",
+            _3 = C6O.p6("J99") ? "Y" : "U";
+        var t = 47. < (0x28, 0xf) ? (37., ",") : (0x27, 0xd) < 64.72E+1 ? (56.9E0, 0xb074319) : ("s", 0x10) <= (96E1, 0x33) ? (0x11, 0xa) : (24, 6) <= "Y" ? 47.9E1 : 0x11 > (16.277E+0, 4) ? 13.01E2 : ("U", 0x48) <= C6O.p6("e8h3") ? "error" : "join" ? 17.8E0 : ("z", 2.11E2) >= ("error", 88.803E1) ? 26.665E1 : (99.558E0, 0x33) < 0x15 ? (91., 61.92E0) : 0x29 > (0x55, 0x5c) ? (0x29, 77.563E1) : (21, 0x20),
+            r = L41[_1](t, 17),
+            n = e;
+        n = this[_2](n, r);
+        var o = L41[_3](n, t);
+        return o
+    },
+    rotateRight: function(e, t) {
+        var _1 = "U",
+            _2 = "z";
+        for (var r, n = 0; L41[_1](t, n); n++) r = L41[_2](1, e), e >>= 1, r <<= 31, e += r;
+        return e
+    },
+    getLiveTkey: function(e, t) {
+        var _1 = C6O.p6("41k2") ? "MD5" : "MD5",
+            _2 = C6O.p6("36N") ? "liveKey" : "MD5",
+            _3 = "join",
+            _4 = ",",
+            _5 = C6O.p6("UX") ? "toString" : "toString";
+        return Encode[_1]([e, t, this[_2]][_3](_4))[_5]()
+    }
+};
+
+function getLetvTKey(t) {
+	return Key.getMmsKey(t);
+}
+
+function test() {
+	return "test js";
+}
